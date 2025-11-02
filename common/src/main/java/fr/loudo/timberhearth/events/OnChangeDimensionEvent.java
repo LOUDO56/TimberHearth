5655dd861c56b4db40c40adb31f34c1659a1552f
LOUDO <lucaslelouer@gmail.com>
2025-11-02 16:56:43 +0100

    Fixed dimension change crash on server

--- common/src/main/java/fr/loudo/timberhearth/events/OnChangeDimensionEvent.java
+++ common/src/main/java/fr/loudo/timberhearth/events/OnChangeDimensionEvent.java
@@ -1,16 +1,27 @@
 package fr.loudo.timberhearth.events;
 
 import fr.loudo.timberhearth.client.TimberHearthSoundControl;
+import fr.loudo.timberhearth.network.PlayTimberHearthSoundS2C;
+import fr.loudo.timberhearth.network.StopTimberHearthSoundS2C;
+import fr.loudo.timberhearth.network.TimberHearthFadeSoundS2C;
+import fr.loudo.timberhearth.platform.Services;
+import fr.loudo.timberhearth.util.UtilCommon;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 
 public class OnChangeDimensionEvent {
 
-    public static void changeDimensionEvent(ResourceKey<Level> resourceKey) {
+    public static void changeDimensionEvent(ResourceKey<Level> resourceKey, ServerPlayer player) {
         if (resourceKey == Level.OVERWORLD) {
-            TimberHearthSoundControl.reset(true, TimberHearthSoundControl.SoundType.JOIN);
+            Services.PACKET_SENDER.sendToPlayer(
+                    player,
+                    new PlayTimberHearthSoundS2C(
+                            UtilCommon.getOSTSecondsFromDayTime(player.level().getDayTime())));
+            Services.PACKET_SENDER.sendToPlayer(
+                    player, new TimberHearthFadeSoundS2C(true, TimberHearthSoundControl.SoundType.DIMENSION));
         } else {
-            TimberHearthSoundControl.stop();
+            Services.PACKET_SENDER.sendToPlayer(player, new StopTimberHearthSoundS2C());
         }
     }
 }
