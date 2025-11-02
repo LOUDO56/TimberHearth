8ac9634277b7eb50f8907cb122e2c93a20f7166f
LOUDO <lucaslelouer@gmail.com>
2025-11-02 14:09:57 +0100

    Fixed music playing on join if day rule false, play music only in overworld

--- common/src/main/java/fr/loudo/timberhearth/mixin/ServerLevelMixin.java
+++ common/src/main/java/fr/loudo/timberhearth/mixin/ServerLevelMixin.java
@@ -6,6 +6,7 @@ import fr.loudo.timberhearth.network.TimberHearthFadeSoundS2C;
 import fr.loudo.timberhearth.platform.Services;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.storage.ServerLevelData;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,6 +31,7 @@ public abstract class ServerLevelMixin {
             method = "tickTime",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setDayTime(J)V"))
     private void timerHeart$overrideTickTime(ServerLevel instance, long time) {
+        if (instance.dimension() != Level.OVERWORLD) return;
         if (time % 24000L >= 23998 || time % 24000L <= 1) {
             server.getPlayerList()
                     .getPlayers()
